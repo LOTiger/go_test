d48c47e7dc665956ca98e1b6201a2717458a38e0
LoTiger <13553480577@163.com>
2019-10-08 22:39:37 +0800

    add:SumAll

--- src/integers/Sum.go
+++ src/integers/Sum.go
@@ -8,3 +8,15 @@ func Sum(numbers []int)  int {
 	}
 	return sum
 }
+
+// SumAll will return sum of slice
+func SumAll(numbersToSum ...[] int) (sums []int)  {
+	lengthOfNumbers := len(numbersToSum)
+	sums = make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return
+}
